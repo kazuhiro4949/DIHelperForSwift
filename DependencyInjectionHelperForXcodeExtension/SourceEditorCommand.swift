305865499109075d13e2b28565d0307992ea3956
kazuhiro4949 <kazuhiro.hayashi.49@gmail.com>
2021-01-17 16:45:35 +0900

    fix

--- DependencyInjectionHelperForXcodeExtension/SourceEditorCommand.swift
+++ DependencyInjectionHelperForXcodeExtension/SourceEditorCommand.swift
@@ -28,7 +28,6 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
 
             for selection in selections {
                 let selectedLines = lines[selection.start.line..<selection.end.line]
-                // SwiftSyntax
                 
                 let sourceFile = try SyntaxParser.parse(source: selectedLines.joined())
                 
@@ -37,63 +36,111 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
                 )
                 extracter.walk(sourceFile)
 
-                var functionTokens = [[String: Any?]]()
-                extracter.functions.forEach { (funcDeclSyntax) in
-                    let identifier = funcDeclSyntax.identifier.text
-//                    let parameterList = funcDeclSyntax.signature.input.parameterList.compactMap { param -> (String, [String])? in
-//                        guard let type = param.type?.as(TypeSyntax.self)?.tokens.map({ $0.text }) else {
-//                            return nil
-//                        }
-//
-//                        let label: String
-//                        switch (param.firstToken?.text, param.secondName?.text) {
-//                        case let (firstText?, secondText?):
-//                            label = "\(firstText) \(secondText)"
-//                        case let (firstText?, nil):
-//                            label = firstText
-//                        case let (nil, secondText?):
-//                            label = secondText
-//                        case (nil, nil):
-//                            label = ""
-//                        }
-//
-//                        return (label, type)
-//                    }
-                    let inputValue = funcDeclSyntax.signature.input.description
-                    let outputValue = funcDeclSyntax.signature.output?.description
-                    
-                    let functionToken: [String: Any?] = [
-                        "identifier": identifier,
-                        "params": inputValue,
-                        "return": outputValue
-                    ]
-                    functionTokens.append(functionToken)
+                let protocolFunctionDecls = extracter.functions.map { (funcDeclSyntax) in
+                    SyntaxFactory.makeFunctionDecl(
+                        attributes: nil,
+                        modifiers: nil,
+                        funcKeyword: funcDeclSyntax.funcKeyword
+                            .withLeadingTrivia(.zero)
+                            .withTrailingTrivia(.spaces(1)),
+                        identifier: funcDeclSyntax.identifier
+                            .withTrailingTrivia(.spaces(1)),
+                        genericParameterClause: nil,
+                        signature: funcDeclSyntax.signature,
+                        genericWhereClause: nil,
+                        body: nil)
                 }
                 
-                let templateText = """
-                protocol {{ identifier }}Protocol {
-                    {%for f in functionList %}
-                    func {{ f.identifier }}{{f.params}}{{f.return}}
-                    {% endfor %}
+                var variables = [VariableDeclSyntax]()
+                extracter.variables.forEach { (variableDeclSyntax) in
+                    if variableDeclSyntax.bindings.count == 1 {
+                        let binding = variableDeclSyntax.bindings.first!
+                        
+                        if let accessorBlock = binding.accessor?.as(AccessorBlockSyntax.self) { // setter, getter
+                            var contextualKeyword: PatternBindingSyntax.ContextualKeyword = []
+                            accessorBlock.accessors.forEach { (accessor) in
+                                if accessor.accessorKind.text == "get" {
+                                    contextualKeyword.insert(.get)
+                                } else if accessor.accessorKind.text == "set" {
+                                    contextualKeyword.insert(.set)
+                                }
+                            }
+                            let protocolVariable = binding.convertForProtocol(with: contextualKeyword)
+                            variables.append(protocolVariable)
+                        } else if binding.accessor?.is(CodeBlockSyntax.self) == true { // computed
+                            let protocolVariable = binding.convertForProtocol(with: .get)
+                            variables.append(protocolVariable)
+                        } else {
+                            let contextualKeyword: PatternBindingSyntax.ContextualKeyword
+                            if variableDeclSyntax.letOrVarKeyword.tokenKind == .letKeyword {
+                                contextualKeyword = .get
+                            } else {
+                                contextualKeyword = [.get, .set]
+                            }
+                            let protocolVariable = binding.convertForProtocol(with: contextualKeyword)
+                            variables.append(protocolVariable)
+                        }
+                    } else {
+                        // let, var
+                        let contextualKeyword: PatternBindingSyntax.ContextualKeyword
+                        if variableDeclSyntax.letOrVarKeyword.tokenKind == .letKeyword {
+                            contextualKeyword = .get
+                        } else {
+                            contextualKeyword = [.get, .set]
+                        }
+                        
+                        let reversedBinding = variableDeclSyntax.bindings.reversed()
+                        var currentTypeAnnotation: TypeAnnotationSyntax?
+                        let protocolVariables = reversedBinding.map { (binidng) -> VariableDeclSyntax in
+                            if let typeAnnotation = binidng.typeAnnotation {
+                                currentTypeAnnotation = typeAnnotation
+                            }
+                            
+                            let bindingWithTypeAnnotation = binidng.withTypeAnnotation(currentTypeAnnotation)
+                            return bindingWithTypeAnnotation.convertForProtocol(with: contextualKeyword)
+                        }
+                        variables.append(contentsOf: protocolVariables)
+                    }
                 }
-                """
-                let template = Template(templateString: templateText)
-                let text = try? template.render([
-                    "identifier" : extracter.identifier!.text,
-                    "functionList": functionTokens
-                ])
-                print(text!)
+
                 
-//                let incrementedLines = incremented.description.lines
-//
-//                let selectedRange = NSRange(
-//                    location: selection.start.line,
-//                    length: selection.end.line - selection.start.line
-//                )
-//                buffer.lines.replaceObjects(
-//                    in: selectedRange,
-//                    withObjectsFrom: incrementedLines
-//                )
+
+                let meberDeclList = SyntaxFactory.makeMemberDeclList(
+                    variables.map {
+                        SyntaxFactory.makeMemberDeclListItem(
+                            decl: DeclSyntax($0)
+                                .withLeadingTrivia(.spaces(4))
+                                .withTrailingTrivia(.newlines(1)),
+                            semicolon: nil)
+                    } +
+                    protocolFunctionDecls.map {
+                    SyntaxFactory.makeMemberDeclListItem(
+                        decl: DeclSyntax($0)
+                            .withLeadingTrivia(.spaces(4))
+                            .withTrailingTrivia(.newlines(1)),
+                        semicolon: nil)
+                    }
+                )
+                
+                let memberDeclBlock = SyntaxFactory.makeMemberDeclBlock(
+                    leftBrace: SyntaxFactory.makeLeftBraceToken()
+                        .withTrailingTrivia(.newlines(1)),
+                    members: meberDeclList
+                        .withTrailingTrivia(.newlines(1)),
+                    rightBrace: SyntaxFactory.makeRightBraceToken())
+                
+                let protocolDecl = SyntaxFactory.makeProtocolDecl(
+                    attributes: nil,
+                    modifiers: nil,
+                    protocolKeyword: SyntaxFactory.makeProtocolKeyword()
+                        .withTrailingTrivia(.spaces(1)),
+                    identifier: extracter.identifier!
+                        .withTrailingTrivia(.spaces(1)),
+                    inheritanceClause: nil, // Anyobject if class
+                    genericWhereClause: nil,
+                    members: memberDeclBlock)
+                
+                print(protocolDecl.description)
             }
         } catch let e {
             print(e)
@@ -115,8 +162,8 @@ class ProtocolExtractor: SyntaxVisitor {
     var keyword: TokenSyntax?
     var identifier: TokenSyntax?
     var functions = [FunctionDeclSyntax]()
-    var variables = [MemberDeclListItemSyntax]()
-    var initilizers = [MemberDeclListItemSyntax]()
+    var variables = [VariableDeclSyntax]()
+    var initilizers = [InitializerDeclSyntax]()
     
     override func visit(_ node: CodeBlockSyntax) -> SyntaxVisitorContinueKind {
         return .visitChildren
@@ -128,22 +175,18 @@ class ProtocolExtractor: SyntaxVisitor {
         functions = node.members.members.compactMap { (member) -> FunctionDeclSyntax? in
             member.decl.as(FunctionDeclSyntax.self)
         }
-        variables = node.members.members.compactMap { (member) -> MemberDeclListItemSyntax? in
-            if member.decl.is(VariableDeclSyntax.self) {
-                return member
-            } else {
-                return nil
-            }
+        variables = node.members.members.compactMap { (member) -> VariableDeclSyntax? in
+            member.decl.as(VariableDeclSyntax.self)
         }
-        initilizers = node.members.members.compactMap { (member) -> MemberDeclListItemSyntax? in
-            if member.decl.is(InitializerDeclSyntax.self) {
-                return member
-            } else {
-                return nil
-            }
+        initilizers = node.members.members.compactMap { (member) -> InitializerDeclSyntax? in
+            member.decl.as(InitializerDeclSyntax.self)
         }
         
         return .skipChildren
+    }
+    
+    override func visit(_ node: ProtocolDeclSyntax) -> SyntaxVisitorContinueKind {
+        .visitChildren
     }
     
 //    override func visit(_ node: StructDeclSyntax) -> SyntaxVisitorContinueKind {
@@ -175,4 +218,82 @@ class ProtocolExtractor: SyntaxVisitor {
         
 //        return .skipChildren
 //    }
+}
+
+extension SyntaxFactory {
+    static func makeAccessorDecl(with contextualKeywordString: String) -> AccessorDeclSyntax {
+        makeAccessorDecl(
+            attributes: nil,
+            modifier: nil,
+            accessorKind: SyntaxFactory.makeToken(
+                .contextualKeyword(contextualKeywordString),
+                presence: .present
+            ),
+            parameter: nil,
+            body: nil)
+    }
+}
+
+extension PatternBindingSyntax {
+    struct ContextualKeyword : OptionSet {
+        let rawValue: UInt
+        
+        static let get = ContextualKeyword(rawValue: 1 << 0)
+        static let set   = ContextualKeyword(rawValue: 1 << 1)
+    }
+    
+    func convertForProtocol(with contextualKeyword: ContextualKeyword) -> VariableDeclSyntax {
+        let accessorDeclSyntaxes: [AccessorDeclSyntax]
+        if contextualKeyword == [.get, .set] {
+            accessorDeclSyntaxes = [
+                SyntaxFactory.makeAccessorDecl(with: "get")
+                    .withLeadingTrivia(.spaces(1))
+                    .withTrailingTrivia(.spaces(1)),
+                SyntaxFactory.makeAccessorDecl(with: "set")
+                    .withTrailingTrivia(.spaces(1))
+            ]
+        } else if contextualKeyword == .get {
+            accessorDeclSyntaxes = [
+                SyntaxFactory.makeAccessorDecl(with: "get")
+                    .withLeadingTrivia(.spaces(1))
+                    .withTrailingTrivia(.spaces(1))
+            ]
+        } else if contextualKeyword == .set {
+            accessorDeclSyntaxes = [
+                SyntaxFactory.makeAccessorDecl(with: "set")
+                    .withLeadingTrivia(.spaces(1))
+                    .withTrailingTrivia(.spaces(1))
+            ]
+        } else {
+            accessorDeclSyntaxes = []
+        }
+        
+        let accessorBlock = SyntaxFactory.makeAccessorBlock(
+            leftBrace: SyntaxFactory.makeLeftBraceToken(),
+            accessors: SyntaxFactory.makeAccessorList(accessorDeclSyntaxes),
+            rightBrace: SyntaxFactory.makeRightBraceToken()
+        )
+        
+        let patternBinding = SyntaxFactory.makePatternBinding(
+            pattern: pattern,
+            typeAnnotation: typeAnnotation,
+            initializer: nil,
+            accessor: Syntax(accessorBlock),
+            trailingComma: nil)
+        
+        let variableDecl = SyntaxFactory.makeVariableDecl(
+            attributes: nil,
+            modifiers: nil,
+            letOrVarKeyword: SyntaxFactory.makeToken(
+                .varKeyword,
+                presence: .present
+            ),
+            bindings: SyntaxFactory.makePatternBindingList([
+                patternBinding
+                    .withLeadingTrivia(.spaces(1))
+                    .withTrailingTrivia(.spaces(1))
+            ])
+        )
+        return variableDecl
+    }
 }
