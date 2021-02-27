1b911027591d3e1672ca497758471842314259f9
kazuhiro4949 <kazuhiro.hayashi.49@gmail.com>
2021-02-27 14:07:17 +0900

    fix

--- UnitTest/ProtocolExtractorTests.swift
+++ UnitTest/ProtocolExtractorTests.swift
@@ -119,24 +119,14 @@ class ProtocolExtractorTests: XCTestCase {
             """
             class FunctionClass {
                 func func1() {
-                    
                 }
-                
                 func func2(arg: Int) {
-                    
                 }
-                
-                
                 func func3(arg: Int, arg2: String, arg3: () -> Void, arg4: (String, String)) {
-                    
                 }
-                
-                
                 func func4() -> String {
                     ""
                 }
-                
-                
                 func func5() throws -> String {
                     ""
                 }
